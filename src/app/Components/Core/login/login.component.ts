eb16d750058a1000e71782669b937b72fccdafc4
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-07 16:56:53 +0200

    Add Login Validation

--- src/app/Components/Core/login/login.component.ts
+++ src/app/Components/Core/login/login.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor(private fb:FormBuilder){}
+
+  fg !:FormGroup;
+
+  IsFormValid = new BehaviorSubject<boolean>(true);
+
+  ngOnInit(): void {
+    this.fg = this.fb.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  OnSubmit(e :Event){
+    e.preventDefault();
+
+    if (this.fg.valid)
+    {
+      this.IsFormValid.next(true);
+    }
+    else
+    {
+      this.IsFormValid.next(false);
+
+    }
+  }
+
+
+
+  // ---------------- [ email ]
+  get emailRequired():boolean|void{return this.fg.get('email')?.hasError('required');}
+  get emailValid(): boolean|void { return this.fg.get('email')?.valid;}
+  get emailTouched():boolean|void{ return this.fg.get('email')?.touched;}
+
+  // ---------------- [ password ]
+  get passRequired():boolean|void{return this.fg.get('password')?.hasError('required');}
+  get passValid(): boolean|void  {return this.fg.get('password')?.valid;}
+  get passTouched():boolean|void {return this.fg.get('password')?.touched;}
 
 }
--- src/app/Components/Core/register/register.component.ts
+++ src/app/Components/Core/register/register.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html',
+  templateUrl:'./register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+
+export class RegisterComponent implements OnInit {
+
+  constructor(private fb:FormBuilder){}
+
+  fg !:FormGroup
+
+  ngOnInit(): void {
+  this.fg = this.fb.group({
+    fullname:['',[Validators.required]],
+    username:['',[Validators.required]],
+    email:   ['',[Validators.required]],
+    password:['',[Validators.required]],
+    confirm: ['',[Validators.required]]
+  })
+  }
+
+  OnSubmit(e :Event){
+    e.preventDefault();
+    if (this.fg.valid)
+    {
+
+    }
+    else
+    {
+
+    }
+  }
+
+
+
+
+  // ---------------- [ email ]
+  get emailRequired():boolean|void{return this.fg.get('email')?.hasError('required');}
+  get emailValid(): boolean|void { return this.fg.get('email')?.valid;}
+  get emailTouched():boolean|void{ return this.fg.get('email')?.touched;}
+
+  // ---------------- [ password ]
+  get passRequired():boolean|void{return this.fg.get('password')?.hasError('required');}
+  get passValid(): boolean|void  {return this.fg.get('password')?.valid;}
+  get passTouched():boolean|void {return this.fg.get('password')?.touched;}
 
 }
