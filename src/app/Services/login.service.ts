9ebfa61397f5bcd4b0fd86c4ffacb8aac85813fd
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-07 23:57:38 +0200

    update login Service

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ILogin } from '../Interfaces/ilogin';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  BaseUrl :string = "https://localhost:7003/api/Accounts/Login"
 
-  Login(user :ILogin){
-    
+  constructor(private httpclien:HttpClient) { }
+
+  Login(user :ILogin): Observable<ILogin>{
+    return this.httpclien.post<ILogin>(this.BaseUrl, user)
   }
 
 }
