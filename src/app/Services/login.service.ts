5b7a73af4872579293044c5be060eae6f095203e
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-09 05:42:13 +0200

    Update Login (show the value of the current user to show it in the navbar)

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,8 +1,11 @@
+import { ClaimsService } from './claims.service';
 import { Injectable } from '@angular/core';
 import { ILogin } from '../Interfaces/ilogin';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Itoken } from '../Interfaces/itoken';
+import { Iclaims } from '../Interfaces/iclaims';
+import { CurrentuserService } from './currentuser.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +13,23 @@ import { Itoken } from '../Interfaces/itoken';
 export class LoginService {
 
   IsLoggedIn = new BehaviorSubject<boolean>(false)
+  CurrentUserName = new BehaviorSubject<string>('')
 
-  BaseUrl :string = "https://localhost:7003/api/Accounts/Login"
+  BaseUrl: string = "https://localhost:7003/api/Accounts/Login"
 
-  constructor(private httpclien:HttpClient) { }
+  constructor(private httpclien: HttpClient, private claim: ClaimsService, private data: CurrentuserService) { }
 
-  Login(user :ILogin): Observable<Itoken>{
-    return this.httpclien.post<Itoken>(this.BaseUrl, user).pipe(tap((res :any)=>
-    {
+  Login(user: ILogin): Observable<Itoken> {
+    return this.httpclien.post<Itoken>(this.BaseUrl, user).pipe(tap((res: any) => {
       if (res)
-      localStorage.setItem('token',res.token)
+      {
+        localStorage.setItem('token', res.token);
+
+        // getting the name of the user to display it in the  navbar
+        let claims = JSON.parse(window.atob(res.token.split('.')[1]));
+        this.CurrentUserName.next(claims[this.claim.claimTypes.GivenName])
+      }
+
       if (localStorage.getItem('token'))
         this.IsLoggedIn.next(true)
     }))
