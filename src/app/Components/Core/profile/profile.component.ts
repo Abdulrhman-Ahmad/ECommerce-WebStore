2b5836ce2bbe722f894681597c697507de068316
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-09 05:44:23 +0200

    modify profile navbar

--- src/app/Components/Core/profile/profile.component.ts
+++ src/app/Components/Core/profile/profile.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClaimsService } from 'src/app/Services/claims.service';
+import { CurrentuserService } from 'src/app/Services/currentuser.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  constructor(public data : CurrentuserService, private claim : ClaimsService){}
+
+ngOnInit(): void {
+  let token = localStorage.getItem('token')
+  if (token) {
+    let claims = JSON.parse(window.atob(token.split('.')[1]));
+    this.data.CurrentUser.GivenName = claims[this.claim.claimTypes.GivenName]
+    this.data.CurrentUser.Name = claims[this.claim.claimTypes.Name]
+    this.data.CurrentUser.StreetAddress = claims[this.claim.claimTypes.StreetAddress]
+    this.data.CurrentUser.MobilePhone = claims[this.claim.claimTypes.MobilePhone]
+    this.data.CurrentUser.Email = claims[this.claim.claimTypes.Email]
+    this.data.CurrentUser.Role = claims[this.claim.claimTypes.Role]
+    }
+
+}
 
 }
