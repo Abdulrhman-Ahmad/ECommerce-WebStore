a167707588f5b3275b4f70aada538c1f7f0d94e7
Tasneem mohamed <tasnemmuhammad37@gmail.com>
2023-11-16 00:50:50 +0200

    Home adminDashboard get values from DB

---
+++ src/app/Services/dashboard/admin-user-manager.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminUserManagerService {
+  baseURL: string = 'https://localhost:7003/api/AdminUserManager';
+  productCountUrl: string = 'https://localhost:7003/api/Products/GetProductsCount';
+  ordersCountUrl: string = 'https://localhost:7003/api/Order/GetOrdersCount';
+  totalSellUrl: string = 'https://localhost:7003/api/Order/TotalSell';
+
+  constructor(private http: HttpClient) {}
+
+  // ---------------- [ Get Users Count ]
+  GetUsersCount() : Observable<number>{
+    return this.http.get<number>(`${this.baseURL}/GetUsersCount`);
+  }
+
+  // ---------------- [ Get Products Count ]
+  GetProductsCount() : Observable<number>{
+    return this.http.get<number>(`${this.productCountUrl}`);
+  }
+
+  // ---------------- [ Get Orders Count ]
+  GetOrdersCount() : Observable<number>{
+    return this.http.get<number>(`${this.ordersCountUrl}`);
+  }
+
+  // ---------------- [ Get Total Sell ]
+  GetTotalSell() : Observable<number>{
+    return this.http.get<number>(`${this.totalSellUrl}`);
+  }
+}
