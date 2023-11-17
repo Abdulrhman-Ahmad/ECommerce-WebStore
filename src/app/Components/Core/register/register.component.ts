0621fcbe5e26626dddd9349c852a152716ca28e2
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-17 15:24:32 +0200

    Ignore .vscode

--- src/app/Components/Core/register/register.component.ts
+++ src/app/Components/Core/register/register.component.ts
@@ -7,13 +7,13 @@ import { RegisterService } from 'src/app/Services/register.service';
 
 @Component({
   selector: 'app-register',
-  templateUrl:'./register.component.html',
+  templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 
 export class RegisterComponent implements OnInit {
 
-  fg !:FormGroup
+  fg !: FormGroup
   IsMatchedPass = new BehaviorSubject<boolean>(false)
 
   user: IRegister = {
@@ -26,26 +26,25 @@ export class RegisterComponent implements OnInit {
   };
 
 
-  constructor(private fb:FormBuilder, private register : RegisterService, private router:Router){}
+  constructor(private fb: FormBuilder, private register: RegisterService, private router: Router) { }
 
 
   ngOnInit(): void {
-  this.fg = this.fb.group({
-    fullname:['',[Validators.required, Validators.minLength(4)]],
-    username:['',[Validators.required, Validators.minLength(4)]],
-    email:   ['',[Validators.required, Validators.email]],
-    password:['',[Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).{8,}$/)]],
-    confirm: ['',[Validators.required, Validators.minLength(8)]],
-    address: ['',[Validators.required, Validators.minLength(8)]],
-    phone:   ['',[Validators.required, Validators.minLength(11)]]
-  })
+    this.fg = this.fb.group({
+      fullname: ['', [Validators.required, Validators.minLength(4)]],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).{8,}$/)]],
+      confirm: ['', [Validators.required, Validators.minLength(8)]],
+      address: ['', [Validators.required, Validators.minLength(8)]],
+      phone: ['', [Validators.required, Validators.minLength(11)]]
+    })
   }
 
-  OnSubmit(e :Event){
+  OnSubmit(e: Event) {
     e.preventDefault();
 
-    if (this.fg.valid && this.IsMatched)
-    {
+    if (this.fg.valid && this.IsMatched) {
       this.IsMatchedPass.next(false)
 
       this.user.fullName = this.fg.get('fullname')?.value;
@@ -59,14 +58,19 @@ export class RegisterComponent implements OnInit {
       this.register.Register(this.user).subscribe(
         {
           next: () => this.router.navigate(['login']),
-          error: () => console.log("Failed To Register!"),
+          error: (e) => {
+
+              e.error.forEach((item: any) => {
+                console.log('Error: ', item.description)
+              });
+
+          },
           complete: () => console.log("Successfully Registered!")
         }
       )
 
     }
-    else
-    {
+    else {
       this.IsMatchedPass.next(true)
     }
   }
@@ -74,40 +78,40 @@ export class RegisterComponent implements OnInit {
 
 
   // ---------------- [ fullname ]
-  get fullnameRequired():boolean|void{return this.fg.get('fullname')?.hasError('required');}
-  get fullnameValid(): boolean|void { return this.fg.get('fullname')?.valid;}
-  get fullnameTouched():boolean|void{ return this.fg.get('fullname')?.touched;}
+  get fullnameRequired(): boolean | void { return this.fg.get('fullname')?.hasError('required'); }
+  get fullnameValid(): boolean | void { return this.fg.get('fullname')?.valid; }
+  get fullnameTouched(): boolean | void { return this.fg.get('fullname')?.touched; }
 
   // ---------------- [ username ]
-  get usernameRequired():boolean|void{return this.fg.get('username')?.hasError('required');}
-  get usernameValid(): boolean|void { return this.fg.get('username')?.valid;}
-  get usernameTouched():boolean|void{ return this.fg.get('username')?.touched;}
+  get usernameRequired(): boolean | void { return this.fg.get('username')?.hasError('required'); }
+  get usernameValid(): boolean | void { return this.fg.get('username')?.valid; }
+  get usernameTouched(): boolean | void { return this.fg.get('username')?.touched; }
 
   // ---------------- [ email ]
-  get emailRequired():boolean|void{return this.fg.get('email')?.hasError('required');}
-  get emailValid(): boolean|void { return this.fg.get('email')?.valid;}
-  get emailTouched():boolean|void{ return this.fg.get('email')?.touched;}
+  get emailRequired(): boolean | void { return this.fg.get('email')?.hasError('required'); }
+  get emailValid(): boolean | void { return this.fg.get('email')?.valid; }
+  get emailTouched(): boolean | void { return this.fg.get('email')?.touched; }
 
   // ---------------- [ password ]
-  get passRequired():boolean|void{return this.fg.get('password')?.hasError('required');}
-  get passValid(): boolean|void  {return this.fg.get('password')?.valid;}
-  get passTouched():boolean|void {return this.fg.get('password')?.touched;}
-  get IsMatched():boolean|void   {return this.fg.get('password')?.value === this.fg.get('confirm')?.value}
+  get passRequired(): boolean | void { return this.fg.get('password')?.hasError('required'); }
+  get passValid(): boolean | void { return this.fg.get('password')?.valid; }
+  get passTouched(): boolean | void { return this.fg.get('password')?.touched; }
+  get IsMatched(): boolean | void { return this.fg.get('password')?.value === this.fg.get('confirm')?.value }
 
   // ---------------- [ confirm ]
-  get confirmRequired():boolean|void{return this.fg.get('confirm')?.hasError('required');}
-  get confirmValid(): boolean|void  {return this.fg.get('confirm')?.valid;}
-  get confirmTouched():boolean|void {return this.fg.get('confirm')?.touched;}
+  get confirmRequired(): boolean | void { return this.fg.get('confirm')?.hasError('required'); }
+  get confirmValid(): boolean | void { return this.fg.get('confirm')?.valid; }
+  get confirmTouched(): boolean | void { return this.fg.get('confirm')?.touched; }
 
   // ---------------- [ address ]
-  get addressRequired():boolean|void{return this.fg.get('address')?.hasError('required');}
-  get addressValid(): boolean|void  {return this.fg.get('address')?.valid;}
-  get addressTouched():boolean|void {return this.fg.get('address')?.touched;}
+  get addressRequired(): boolean | void { return this.fg.get('address')?.hasError('required'); }
+  get addressValid(): boolean | void { return this.fg.get('address')?.valid; }
+  get addressTouched(): boolean | void { return this.fg.get('address')?.touched; }
 
   // ---------------- [ phone ]
-  get phoneRequired():boolean|void{return this.fg.get('phone')?.hasError('required');}
-  get phoneValid(): boolean|void  {return this.fg.get('phone')?.valid;}
-  get phoneTouched():boolean|void {return this.fg.get('phone')?.touched;}
+  get phoneRequired(): boolean | void { return this.fg.get('phone')?.hasError('required'); }
+  get phoneValid(): boolean | void { return this.fg.get('phone')?.valid; }
+  get phoneTouched(): boolean | void { return this.fg.get('phone')?.touched; }
 
 
 }
