435e05e76328f5e4543166f4b6bf6ff865858254
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-19 19:16:07 +0200

    Update Checkout, Add interfaces

--- src/app/Interfaces/iaddress.ts
+++ src/app/Interfaces/iaddress.ts
@@ -2,8 +2,8 @@ export interface Iaddress {
   id : 0,
   street : string,
   city : string,
-  state : string,
   country : string,
+  state : string,
   postalCode : 0,
   specialInstructions :  string
 }
