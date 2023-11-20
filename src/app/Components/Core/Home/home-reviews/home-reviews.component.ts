131cb1d2b85b65b872ce72b748aef8423c6b779b
Tasneem mohamed <tasnemmuhammad37@gmail.com>
2023-11-20 20:30:50 +0200

    Top3 products & What Our Customers Are Saying

--- src/app/Components/Core/Home/home-reviews/home-reviews.component.ts
+++ src/app/Components/Core/Home/home-reviews/home-reviews.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Ireviewreturn } from 'src/app/Interfaces/review/Ireviewreturn';
+import { ReviewService } from 'src/app/Services/review.service';
 
 @Component({
   selector: 'app-home-reviews',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomeReviewsComponent {
 
+  reviews:Ireviewreturn[] = [];
+  ProductsImg: string[] = [
+    'assets/Images/blog1.jpg' ,
+    'assets/Images/blog2.jpg' ,
+    'assets/Images/blog3.jpg' ,
+    'assets/Images/blog4.jpg'
+  ];
+
+
+  constructor(private reviewService:ReviewService){}
+
+  ngOnInit(): void {
+    this.reviewService.GetAll().subscribe({
+      next:(data) =>{this.reviews= data},
+      error:(error)=>{console.log('error'+error)},
+      complete: ()=>{},
+    });
+    
+    
+  }
 }
