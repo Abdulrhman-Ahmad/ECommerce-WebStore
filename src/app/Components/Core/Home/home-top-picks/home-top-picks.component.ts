19cb08909f2b7f117c60cbc1bd3bf19b4a36fa8f
Tasneem mohamed <tasnemmuhammad37@gmail.com>
2023-11-20 19:41:33 +0200

    Top Picks

--- src/app/Components/Core/Home/home-top-picks/home-top-picks.component.ts
+++ src/app/Components/Core/Home/home-top-picks/home-top-picks.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Iproduct } from 'src/app/Interfaces/iproduct';
+import { ProductlistService } from 'src/app/Services/productlist.service';
 
 @Component({
   selector: 'app-home-top-picks',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-top-picks.component.css']
 })
 export class HomeTopPicksComponent {
+  products !: Iproduct[];
+  constructor(private productlist: ProductlistService) { }
+  ngOnInit(): void {
+
+    // Load all the products only the first index
+    this.productlist.GetTop3Product().subscribe(
+      {
+        next: (data) => this.products = data,
+        error: (e) => console.log('Error: ', e),
+        complete: () => console.log("Got Data Successfully!")
+      }
+    );
+
+  }
 
 }
